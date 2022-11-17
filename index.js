b5fb4e6f59e3ed1a0afe925d2ca818710b4770a9
Mohammad Sumon <sumon3011@gmail.com>
2022-11-17 18:05:45 +0600

    module 75.5 done

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send('unauthorized access');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'forbidden access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
 async function run() {
   try {
     const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
@@ -113,9 +131,14 @@ async function run() {
      */
 
 
-    app.get('/bookings', async(req, res) => {
+    app.get('/bookings', verifyJWT, async(req, res) => {
       const email = req.query.email;
-      // console.log(email);
+      const decodedEmail = req.decoded.email;
+
+      if(email !== decodedEmail){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
       const query = {email: email};
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
