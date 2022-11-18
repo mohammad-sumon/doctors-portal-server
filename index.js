05e01ec72b02efdb01b2013563e73b9cf5f16370
mohammad-sumon <sumon3011@gmail.com>
2022-11-18 21:53:16 +0600

    module 76.3 done

--- index.js
+++ index.js
@@ -124,6 +124,15 @@ async function run() {
       res.send(options);
     });
 
+    app.get("/appointmentSpecialty", async (req, res) => {
+      const query = {};
+      const result = await appointmentOptionCollection
+        .find(query)
+        .project({ name: 1 })
+        .toArray();
+      res.send(result);
+    });
+
     /**
      * API naming convention:
      * ----------------------
